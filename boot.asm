62003cd0ac0779d39ece46bbd32421dd91066eb8
Alex Striff <alex.striff1@gmail.com>
2016-06-28 22:49:01 -0700

    Fix: Splash screen now uses 8x less memory (bitmap image overlay)

--- boot.asm
+++ boot.asm
@@ -222,8 +222,9 @@ after_bootsector:		; should start at 0x7e00
 	mov ax, 0x0e02		; teletype output, smiley
 	int 0x10
 
+	; TODO: Check available video modes and skip splash if not supported
 	; set video mode
-	mov ax, 0x0013		; set video mode, 320x200 256-color VGA mode
+video:	mov ax, 0x0013		; set video mode, 320x200 256-color VGA mode
 	int 0x10
 	mov ax, 0x0500		; set active display page, page to display
 	int 0x10
@@ -231,31 +232,14 @@ after_bootsector:		; should start at 0x7e00
 	; clear and attribute screen with video memory manipulation
 	mov ax, 0xa000		; 0x000a0000 is video memory in VGA
 	mov es, ax		; set segment
-	mov bl, 0x04		; debug color
-	; mov bl, 0x3e		; as close as VGA can get to *the* top pink
+	mov bl, 0x3e		; as close as VGA can get to *the* top pink
 	mov cx, (320 * 200)	; pixels 640x480 vga video display
 .pink:	mov si, cx
 	mov byte [es:0+si-1], bl	; write to video memory
 	loop .pink
 
-	; ; read in the splash screen bitmap
-	; mov ax, 2060		; for now, from a hardcoded block
-	; mov [dap.startblk], ax
-	; xor ax, ax
-	; mov [dap.startblk+2], ax
-	; mov ax, 0x8000		; for now, to a hardcoded memory location
-	; mov [dap.transbuf], ax
-	; xor ax, ax
-	; mov [dap.transbuf+2], ax
-	; mov ah, 0x42		; extended read
-	; mov dl, [ds:disk]	; some BIOSes trash dx, so read the num again
-	; mov si, dap		; ds is 0, so ds:si is right disk address packet
-	; int 0x13
-
-	; read in the splash screen image
-	mov ax, 125		; The image (VGA memory) is 64,000 bytes
-	mov [dap.blocks], ax
-	mov ax, 2076		; for now, from a hardcoded block
+	; read in the splash screen bitmap
+	mov ax, 2060		; for now, from a hardcoded block
 	mov [dap.startblk], ax
 	xor ax, ax
 	mov [dap.startblk+2], ax
@@ -268,19 +252,38 @@ after_bootsector:		; should start at 0x7e00
 	mov si, dap		; ds is 0, so ds:si is right disk address packet
 	int 0x13
 
-	push ds
-	mov cx, (320 * 200) / 2	; words in the bitmap
-	mov ax, 0xa000		; 0x000a0000 is video memory in VGA
-	mov es, ax		; set segment
-	mov ax, 0x0800		; set ds to be 0x0800 (loaded image base)
-	mov ds, ax		; set segment
-	xor si, si		; src
-	xor di, di		; dest
-	rep movsw		; copy the memory
-	pop ds
-
-	; TODO: convert a bit in the bitmap to a bottom pink byte in VGA memory!
+	; TODO: map each bit of the read bitmap to a byte in video memory
+	; the bitmap goes from high to low, so bit 7 of the first byte is the
+	; first pixel in the image, and bit 0 of the first byte is the 8th pixel
+	; in the image
 	; mov bl, 0x54		; as close as VGA can get to *the* bottom pink
+	push ds
+	xor si, si		; zero si
+	mov ax, 0xa000		; video memory segment
+	mov es, ax		; set segment for video mem writes
+	mov ax, 0x0800		; loaded bitmap memory location (segment)
+	mov ds, ax		; set segment for lodsb
+
+.bmp:	lodsb			; load the byte at [ds:si] into al
+	mov ah, al		; save byte
+
+	xor bx, bx		; index of bit in the byte
+.bit:	mov dl, 0x54		; default out byte is the bottom pink
+	mov al, ah		; restore byte
+	and al, 10000000b	; check bit 7 (leftmost bit)
+	jz .nobit		; 0 means do not set the color
+	mov di, si		; copy si
+	dec di			; si is incremented by lodsb, so "undo"
+	shl di, 3		; multiply by 8
+	mov [es:bx+di], dl	; write the byte
+.nobit:	shl ah, 1		; next bit
+	inc bx			; index of next bit
+	cmp bx, 8
+	jb .bit
+
+	cmp si, (320 * 200) / 8	; size of bitmap (in bytes)
+	jb .bmp
+	pop ds
 
 	cli
 .halt:	hlt
@@ -358,7 +361,6 @@ detect_a20:
 
 ;;;;;;;; END EXTENDED SUBROUTINES ;;;;;;;;
 
-	; times 508-($-after_bootsector) nop	; fill remaining with nops
 	align 512		; fill remaining sector with nops
 
 ;;;;;;;; END AFTER BOOTSECTOR CODE ;;;;;;;;
